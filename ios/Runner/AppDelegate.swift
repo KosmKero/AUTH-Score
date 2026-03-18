8347f7cb43cd2b87d7bb3859c94abd37dc10c791
Kosmas Keramopoulos <kosmaskeramopoulos@gmail.com>
2026-03-18 12:44:44 +0200

    please ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import FirebaseCore // Προσθήκη
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,12 +7,6 @@ import FirebaseCore // Προσθήκη
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // ΠΡΟΣΘΗΚΗ: Αρχικοποιούμε το Firebase στο Native επίπεδο ΠΡΙΝ τα plugins
-    if FirebaseApp.app() == nil {
-        FirebaseApp.configure()
-    }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
