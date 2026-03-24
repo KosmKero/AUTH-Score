9487a9b63fadf04f0823f072c9b3013b47fde569
Kosmas Keramopoulos <kosmaskeramopoulos@gmail.com>
2026-03-24 12:24:48 +0200

    main piso

--- lib/main.dart
+++ lib/main.dart
@@ -120,8 +120,8 @@ void main() async {
     //await MatchHandle.migrateMatches();
     //await MatchHandle.migrateTeams();
     //await MatchHandle().resetPlayerData("2026");
-    await Future.delayed(const Duration(milliseconds: 100)); //να προλαβουν να γινουν ολα σωστα
     await Future.delayed(
+        const Duration(milliseconds: 100)); //να προλαβουν να γινουν ολα σωστα
 
     User? user = FirebaseAuth.instance.currentUser;
      await initTracking(); //προβλημα
